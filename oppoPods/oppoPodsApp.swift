ea95585f49db9664ad3d3f213cc83e186754fded
没有用的安龙 <152011199+aurysian-yan@users.noreply.github.com>
2026-06-10 21:05:21 +0800

    Initial Commit

---
+++ oppoPods/oppoPodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  oppoPodsApp.swift
+//  oppoPods
+//
+//  Created by Aurysian on 2026/6/10.
+//
+
+import SwiftUI
+
+@main
+struct oppoPodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
